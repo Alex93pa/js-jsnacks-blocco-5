c75f9a08a5cd49df7047512192fd6f0469c68fc6
Alex93pa <Ale.pug2@gmail.com>
2021-04-23 16:11:17 +0200

    primo finito

---
+++ snack1/script.js
@@ -0,0 +1,65 @@
+var zucchine = [
+    {
+        varieta : "a",
+        peso : 200,
+        lunghezza : 13,
+    },
+    {
+        varieta : "b",
+        peso : 310,
+        lunghezza : 22,
+    },
+    {
+        varieta : "c",
+        peso : 110,
+        lunghezza : 9,
+    },
+    {
+        varieta : "d",
+        peso :200,
+        lunghezza : 14,
+    },
+    {
+        varieta : "e",
+        peso : 330,
+        lunghezza : 21,
+    },
+    {
+        varieta : "f",
+        peso : 300,
+        lunghezza : 18,
+    },
+    {
+        varieta : "g",
+        peso : 600,
+        lunghezza : 32,
+    },
+    {
+        varieta : "h",
+        peso : 200,
+        lunghezza : 20,
+    },
+    {
+        varieta : "i",
+        peso : 170,
+        lunghezza : 13,
+    },
+    {
+        varieta : "l",
+        peso : 190,
+        lunghezza : 14,
+    },
+];
+
+function pesoTotale (zucchine){
+    var pesoTotale = 0;
+    
+    for (var i = 0; i < zucchine.length; i++) {
+        var pesoSingolo = zucchine[i].peso;
+        pesoTotale += pesoSingolo;
+    };
+    return pesoTotale;
+}
+
+console.log("Il peso totale delle zucchine è " + pesoTotale(zucchine)
++"gr.")
